5729486ab47488faf3d85ba8adf4f8826051ccd9
Arion Dantas Jr <94414568+ArionDantas@users.noreply.github.com>
2023-06-15 12:58:49 +0000

    tela de login admin higia

--- js/admin/login.js
+++ js/admin/login.js
@@ -1,14 +1,13 @@
 const togglePassword = document.getElementById("acessarSenha");
-const password = document.querySelector("#password");
+const password = document.getElementById("acessarSenha");
+const inputSenha = document.getElementById("input-senha");
 
-console.log(togglePassword);
 
 if (togglePassword) {
     togglePassword.addEventListener("click", function () {
-
         // toggle the type attribute
-        const type = password.getAttribute("type") === "password" ? "text" : "password";
-        password.setAttribute("type", type);
+        const type = inputSenha.getAttribute("type") === "password" ? "text" : "password";
+        inputSenha.setAttribute("type", type);
         // toggle the eye icon
         this.classList.toggle('fa-eye');
         this.classList.toggle('fa-eye-slash');
